e4d71c9dbcbf82fce175cc3fc1929b8e2f7303e4
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-05-23 16:43:14 -0700

    waitforboulder

--- tests/travis-integration.sh
+++ tests/travis-integration.sh
@@ -10,10 +10,9 @@ export CERTBOT_PATH=`pwd`
 
 cd $GOPATH/src/github.com/letsencrypt/boulder/
 
-# boulder's integration-test.py has code that knows to start and wait for the
-# boulder processes to start reliably and then will run the certbot
-# boulder-interation.sh on its own. The --certbot flag says to run only the
-# certbot tests (instead of any other client tests it might run). We're
-# going to want to define a more robust interaction point between the boulder
-# and certbot tests, but that will be better built off of this.
-python test/integration-test.py --certbot
+until curl http://boulder:4000/directory ; do
+  echo waiting for boulder
+  sleep 1
+done
+
+./boulder-integration.sh
