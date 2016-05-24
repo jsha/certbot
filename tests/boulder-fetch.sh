eb6a387fe1e6ce617be73387be3b547cb99fb469
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-05-23 17:22:55 -0700

    fakedns2

--- tests/boulder-fetch.sh
+++ tests/boulder-fetch.sh
@@ -6,4 +6,6 @@ set -xe
 # see `go help packages`
 git clone -b rev-rev https://github.com/letsencrypt/boulder $BOULDERPATH
 cd $BOULDERPATH
+sed -i 's/FAKE_DNS: 127.0.0.1/FAKE_DNS: 172.17.0.1/' docker-compose.yml
 docker-compose up -d
+
--- tests/letstest/scripts/boulder_install.sh
+++ tests/letstest/scripts/boulder_install.sh
@@ -5,4 +5,5 @@
 # install boulder and its go dependencies
 git clone -b rev-rev https://github.com/letsencrypt/boulder $BOULDERPATH
 cd $BOULDERPATH
+sed -i 's/FAKE_DNS: 127.0.0.1/FAKE_DNS: 172.17.0.1/' docker-compose.yml
 docker-compose up -d
