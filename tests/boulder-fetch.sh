f08bab06e891ec222ad550b7d233408bd2836042
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-05-23 17:36:50 -0700

    42

--- tests/boulder-fetch.sh
+++ tests/boulder-fetch.sh
@@ -7,6 +7,6 @@ set -xe
 git clone -b rev-rev https://github.com/letsencrypt/boulder $BOULDERPATH
 cd $BOULDERPATH
 ifconfig
-sed -i 's/FAKE_DNS: 127.0.0.1/FAKE_DNS: 172.17.0.1/' docker-compose.yml
+sed -i 's/FAKE_DNS: 127.0.0.1/FAKE_DNS: 172.17.42.1/' docker-compose.yml
 docker-compose up -d
 
