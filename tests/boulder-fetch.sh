7c08d49cc1fa6b15bdfadc90fa144abb17942c56
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-08-10 11:18:49 -0700

    List interfaces.

--- tests/boulder-fetch.sh
+++ tests/boulder-fetch.sh
@@ -5,6 +5,7 @@ set -xe
 # Check out special branch until latest docker changes land in Boulder master.
 git clone -b docker-integration https://github.com/letsencrypt/boulder $BOULDERPATH
 cd $BOULDERPATH
+ifconfig
 sed -i 's/FAKE_DNS: .*/FAKE_DNS: 172.17.42.1/' docker-compose.yml
 docker-compose up -d
 
